85c53d88b14efbe70f7deb475304d3b932fee843
kethanpabbi <kethanpabbi@gmail.com>
2021-10-09 21:52:23 +0100

    Update Exponential.R

--- examples/Exponential.R
+++ examples/Exponential.R
@@ -1,5 +1,10 @@
-# Exponential funtion 
+#' Exponential funtion 
+#' 
+#'
+#' @kethan
 set.seed(4060)
 N = 1000
 x = rexp(N, rate=.5)
 hist(x, col = 3,freq = F)
+
+devtools::install_github("kethanpabbi/R")
